638b20bd10cff7a7a81c1c34d06e389e97eb751c
gv4383 <gregory.vargas18@gmail.com>
2018-10-11 17:57:41 -0500

    App component renders elements in list array in state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Vue To-Do List</h1>
+    <main>
+      <p :key="item" v-for="item in list">{{ item }}</p>
+    </main>
   </div>
 </template>
 
@@ -10,6 +14,11 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
+  data() {
+    return {
+      list: ["Hello", "World"]
+    };
+  },
   components: {
     HelloWorld
   }
@@ -17,12 +26,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./sass/main";
 </style>
