4cbe5d56f5ced420308c05a6de4abe6d47279a30
gv4383 <gregory.vargas18@gmail.com>
2018-10-12 18:18:16 -0500

    Removes list items when list item is clicked on

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
       <div class="title">
         <img alt="Vue logo" src="./assets/logo.png">
         <h1>Vue To-Do List</h1>
-        <input v-model="userInput" placeholder="Type a new to-do">
+        <input v-model="userInput" placeholder="Type a new to-do" type="text">
         <button @click="addToList">Submit</button>
       </div>
       <main>
-        <p :key="index" v-for="(item, index) in list">{{ item }}</p>
+        <p :key="index" v-for="(item, index) in list" @click="removeFromList(index)">{{ item }}</p>
       </main>
     </div>
   </div>
@@ -23,13 +23,16 @@ export default {
   data() {
     return {
       userInput: "",
-      list: ["Hello", "World"]
+      list: ["Hello", "World!", "Click me to remove me from the list!"]
     };
   },
   methods: {
     addToList: function() {
       this.list.push(this.userInput);
       this.userInput = "";
+    },
+    removeFromList: function(i) {
+      this.list.splice(i, 1);
     }
   },
   components: {
